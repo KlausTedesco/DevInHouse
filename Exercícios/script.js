3ff15c76f79f546c665ba3cf911aa7f4b732a118
klaus.tedesco <klaus.tedesco@gmail.com>
2020-11-25 09:55:59 -0300

    Exercício 2 - Semana 2

--- Exercícios/script.js
+++ Exercícios/script.js
@@ -1,1 +1,10 @@
-alert("Este site tem como finalidade resolver exercicios do curso.");
+alert("Este site tem como finalidade resolver exercicios do curso DevInHouse.");
+
+function changePage(){
+    var change = confirm("Você tem certeza que deseja trocar de página?");
+    if( change == true ){
+        location.href = "images.html";
+    } else{
+        location.href = "index.html";
+    }
+ }
