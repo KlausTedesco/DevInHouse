0cc63160bdea7ac2109470c1d775c5103b41d055
klaus.tedesco <klaus.tedesco@gmail.com>
2020-12-10 21:10:17 -0300

    Exercício 4 - Semana 4

--- QuartaSemana/ExercicioTresEQuatro/script.js
+++ QuartaSemana/ExercicioTresEQuatro/script.js
@@ -4,29 +4,38 @@
 // e descubra se alguma das pessoas da lista é maior de idade (18 anos) 
 // retornando o resultado.
 
+// 4 - Verificação de Profissão
+// Considerando a mesma entidade Pessoa do exercício 3: https://trello.com/c/LgeM2xcO 
+// crie uma função que receba uma lista de pessoas e descubra se todas as pessoas da lista 
+// possuem a profissão “Programador” retornando o resultado.
+
 const personOne = {
     name:'Klaus', 
     age:'28', 
     phone:'048999999999', 
-    occupation: 'Developer'
+    occupation: 'Programador'
 };
 
 const personTwo = {
     name:'Demétrio', 
     age:'7', 
     phone:'048999999999', 
-    occupation: 'Student'
+    occupation: 'Estudante'
 };
 
 const personThree = {
     name:'Frederico', 
     age:'2', 
     phone:'048999999999', 
-    occupation: 'Student'
+    occupation: 'Estudante'
 };
 
 const listPeople = [ personOne, personTwo, personThree ]
 
 function checkAdult( listPeople ){
     return listPeople.some( person => person.age >= 18 ) ;
+}
+
+function checkOccupation( listPeople ){
+    return listPeople.every( person => person.occupation === 'Programador' ) ;
 }
