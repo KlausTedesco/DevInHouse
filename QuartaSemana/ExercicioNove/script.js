efe4ab4081576d8b7c7b0d0c32a5ba661fd66e64
klaus.tedesco <klaus.tedesco@gmail.com>
2020-12-13 10:07:56 -0300

    Exercício 9 - Semana 4

---
+++ QuartaSemana/ExercicioNove/script.js
@@ -0,0 +1,7 @@
+// 9 - Apresentação de usuário
+// Declare uma arrowFunction que deverá possuir dois parâmetros (nome, idade). 
+// A nossa função deve mostrar no console a seguinte frase: “Olá, eu sou <nome>, 
+// e tenho <idade> anos”.
+const presentationPerson = ( name, age ) => {
+    return 'Olá, eu sou ' + name + ', e tenho ' + age + ' anos';
+}
